51e6c75e5634689a34ee98c3e36fdeaf009ef4c1
Avinash S <105902235+Avinash8055@users.noreply.github.com>
2025-03-14 17:42:24 +0530

    Delete Task_Master/capacitor.config.ts

--- Task_Master/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'task_master.example.app',
-  appName: 'Task_Master',
-  webDir: 'dist'
-};
-
-export default config;
